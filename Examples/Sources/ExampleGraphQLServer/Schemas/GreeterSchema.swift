b109d3605147c21ff4e5a1bba2a1d946195df49b
Sami Suteria <sami@no.dev>
2023-12-15 11:31:52 -0600

    update example to include custom date encoding and decoding

--- Examples/Sources/ExampleGraphQLServer/Schemas/GreeterSchema.swift
+++ Examples/Sources/ExampleGraphQLServer/Schemas/GreeterSchema.swift
@@ -1,12 +1,19 @@
+import Foundation
 import Graphiti
 import ServerKit
 
 final class GreeterSchema: GraphQLPartialSchema {
+    @TypeDefinitions
+    public override var types: Types {
+        Scalar(Date.self, as: "DateTime")
+    }
+
     @FieldDefinitions
     public override var query: Fields {
         Field("greet", at: GraphQLResolver.greet) {
             Argument("name", at: \.name)
         }
+        Field("now", at: GraphQLResolver.now)
     }
 }
 
@@ -17,5 +24,9 @@ extension GraphQLResolver {
 
     func greet(context: GraphQLContext, args: GreetArguments) -> String {
         return "Hello \(args.name)"
+    }
+
+    func now(context: GraphQLContext, args: NoArguments) -> Date {
+        return Date.now
     }
 }
--- Examples/Sources/ExampleGraphQLServer/main.swift
+++ Examples/Sources/ExampleGraphQLServer/main.swift
@@ -5,7 +5,12 @@ import Vapor
 let environment = try Environment.detect()
 let application = Application(environment)
 
+let coders = Coders()
+coders.decoder.dateDecodingStrategy = .iso8601
+coders.encoder.dateEncodingStrategy = .iso8601
+application.graphql.configuration.use(coders: coders)
 application.graphql.configuration.use(partial: GreeterSchema())
+
 application.graphql.server.configuration.address = .hostname("0.0.0.0", port: 4001)
 try application.graphql.configureApplication()
 
